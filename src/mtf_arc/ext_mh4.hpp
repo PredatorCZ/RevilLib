4df32b1e5b112840c432fb9747b190f2bf5ec257
Lukas Cone <lukas.kuzel.ch@gmail.com>
2021-03-20 22:44:41 +0100

    Added more extensions

---
+++ src/mtf_arc/ext_mh4.hpp
@@ -0,0 +1,67 @@
+/*  Revil Format Library
+    Copyright(C) 2021 Lukas Cone
+
+    This program is free software : you can redistribute it and / or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "ext_base.hpp"
+
+static const MtExtensionsStorage extMH4Common{
+    {"ase", 0x07437CCE},     //
+    {"ccl", 0x0026E7FF},     //
+    {"cfl", 0x7BEA3086},     //
+    {"ctc", 0x535D969F},     //
+    {"ean", 0x4E397417},     //
+    {"efl", 0x6D5AE854},     //
+    {"emc", 0x3AABBA02},     //
+    {"emd", 0xA0E48D4},      //
+    {"etd", 0x3D8BBBAC},     //
+    {"evt", 0x60869A71},     //
+    {"gr2", 0x11C35522},     //
+    {"gr2s", 0x628DFB41},    //
+    {"grw", 0x0437BCF2},     //
+    {"ipl", 0x25FD693F},     //
+    {"ips", 0x52776AB1},     //
+    {"lanl", 0x708E0028},    //
+    {"lcm", 0x39C52040},     //
+    {"lfd", 0x3516C3D2},     //
+    {"lmd", 0x62440501},     //
+    {"lmt", 0x76820D81},     //
+    {"lyt", 0x15302EF4},     //
+    {"mca", 0x67195A2E},     //
+    {"mef", 0x148B6F89},     //
+    {"mod", 0x58A15856},     //
+    {"mrl", 0x2749C8A8},     //
+    {"mss", 0x6E171A6E},     //
+    {"quest", 0x1BBFD18E},   //??
+    {"rev_ctr", 0x4A4B677C}, //
+    {"sai", 0x2A8800EE},     //
+    {"sbc", 0x51FC779F},     //
+    {"sbk", 0x14B5C8E6},    //
+    {"scd", 0x6B41A2F9},     //
+    {"scs", 0x0ECD7DF4},     //
+    {"sdl", 0x4C0DB839},     //
+    {"sds", 0x0315E81F},     //
+    {"ses", 0x68CD2933},     //
+    {"sis", 0x65375D5},      //
+    {"skmt", 0x70C56D5E},    //
+    {"sksg", 0x2C59EEA9},    //
+    {"skst", 0x19278D07},    //
+    {"srq", 0x2618DE3F},     //
+    {"stq", 0x3A6A5A4D},     //
+    {"tex", 0x241F5DEB},     //
+};
+
+static const MtExtensions extMH4{&extMH4Common, Platform::N3DS};
---
+++ src/mtf_arc/ext_pwaasoj.hpp
@@ -0,0 +1,50 @@
+/*  Revil Format Library
+    Copyright(C) 2020 Lukas Cone
+
+    This program is free software : you can redistribute it and / or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "ext_base.hpp"
+
+static const MtExtensionsStorage extPWAASOJCommon{
+    {"ccl", 0x0026E7FF},  //
+    {"cli", 0x6D96778B},  //
+    {"ctc", 0x535D969F},  //
+    {"dsc", 0x75967AD6},  //
+    {"e2d", 0x276DE8B7},  //
+    {"ean", 0x4E397417},  //
+    {"efl", 0x6D5AE854},  //
+    {"fng", 0x64A7CFA9},  //
+    {"gfd", 0x2D462600},  //
+    {"gii", 0x07F768AF},  //
+    {"gmd", 0x242BB29A},  //
+    {"gui", 0x22948394},  //
+    {"chr", 0x194BE415},  //
+    {"lcm", 0x39C52040},  //
+    {"lmt", 0x76820D81},  //
+    {"lpm", 0x0C41C74D},  //
+    {"mca", 0x79C47B59},  //
+    {"mhd", 0x2C97686F},  //
+    {"mod", 0x58A15856},  //
+    {"mrl", 0x2749C8A8},  //
+    {"sbc", 0x51FC779F},  //
+    {"sbkr", 0x15D782FB}, //
+    {"sdl", 0x4C0DB839},  //
+    {"srqr", 0x1BCC4966}, //
+    {"stqr", 0x167DBBFF}, //
+    {"tex", 0x241F5DEB},  //
+};
+
+static const MtExtensions extPWAASOJ{&extPWAASOJCommon, Platform::N3DS};
