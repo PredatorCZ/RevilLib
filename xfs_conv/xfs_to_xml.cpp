1cd2aba53895375964dcf6a07d130b18d461818f
Lukas Cone <lukas.kuzel.ch@gmail.com>
2021-09-27 16:59:07 +0200

    ass xfs_conv

---
+++ xfs_conv/xfs_to_xml.cpp
@@ -0,0 +1,56 @@
+/*  XFSConvert
+    Copyright(C) 2021 Lukas Cone
+
+    This program is free software : you can redistribute it and / or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "datas/binreader_stream.hpp"
+#include "datas/pugiex.hpp"
+#include "project.h"
+#include "re_common.hpp"
+#include "revil/xfs.hpp"
+#include <algorithm>
+
+es::string_view filters[]{
+    {},
+};
+
+struct XFS2XML : ReflectorBase<XFS2XML> {
+  bool saveRTTI = true;
+  bool saveData = true;
+} settings;
+
+REFLECT(CLASS(XFS2XML),
+        MEMBERNAME(saveRTTI, "save-rtti", "r",
+                   ReflDesc{"Save layout information."}),
+        MEMBERNAME(saveData, "save-data", "d", ReflDesc{"Save data."}), );
+
+ES_EXPORT AppInfo_s appInfo{
+    AppInfo_s::CONTEXT_VERSION,
+    AppMode_e::CONVERT,
+    ArchiveLoadType::ALL,
+    XFSConvert_DESC " v" XFSConvert_VERSION ", " XFSConvert_COPYRIGHT
+                    "Lukas Cone",
+    reinterpret_cast<ReflectorFriend *>(&settings),
+    filters,
+};
+
+void AppProcessFile(std::istream &stream, AppContext *ctx) {
+  XFS xfs;
+  xfs.Load(stream);
+  auto outFile = ctx->workingFile + ".xml";
+  pugi::xml_document doc;
+  xfs.ToXML(doc);
+  XMLToFile(outFile, doc);
+}
